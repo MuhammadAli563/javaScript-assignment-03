1fd60d0951a35b0316611367509940d18c71da17
Muhammad Ali <03152214619ali@gmail.com>
2023-03-25 22:09:04 +0500

    javascript assignment 3

---
+++ app.js
@@ -0,0 +1,39 @@
+// CHAPTER 14 - 16
+
+// Q#01
+// var numArr = ["","",""]
+// console.log(numArr)
+
+// Q#02
+// var numArr = new["","",""]
+// console.log(numArr)
+
+// Q#03
+// var numArr = ["bed","sofa","cussion"]
+// console.log(numArr)
+
+// Q#04
+// var numArr = [1,2,3,4,5]
+// console.log(numArr)
+
+// Q#05
+// var numArr = [True,False]
+// console.log(numArr)
+
+// Q#06
+// var numArr = ["apple",2,true,]
+// console.log(numArr)
+
+// Q#07
+// var numArr = ["SSC","HSC", "BCS","BS","BCOM", "MS", "M.Phil", "PhD"]
+// console.log(numArr[0])
+// console.log(numArr[1])
+// console.log(numArr[2])
+// console.log(numArr[3])
+// console.log(numArr[4])
+// console.log(numArr[5])
+// console.log(numArr[6])
+// console.log(numArr[7])
+
+// Q#08
+
